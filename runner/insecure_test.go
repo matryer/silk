a3a656a2d91cce4d6bcdce7733789ad003ee262d
juacompe <juacompe@gmail.com>
2018-02-17 08:19:14 +0700

    Extract TestT out (duplicated with runner_test)

--- runner/insecure_test.go
+++ runner/insecure_test.go
@@ -3,10 +3,8 @@
 package runner
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"github.com/cheekybits/is"
@@ -15,7 +13,7 @@ import (
 
 func TestAllowConnectionsToSSLSitesWithoutCerts(t *testing.T) {
 	is := is.New(t)
-	subT := &testT{}
+	subT := &testutil.TestT{}
 	s := httptest.NewServer(testutil.EchoRawHandler())
 	defer s.Close()
 	r := New(subT, s.URL)
@@ -23,25 +21,4 @@ func TestAllowConnectionsToSSLSitesWithoutCerts(t *testing.T) {
 	req, _ := http.NewRequest("GET", "https://untrusted-root.badssl.com", nil)
 	_, err := r.DoRequest(req)
 	is.Nil(err)
-}
-
-type testT struct {
-	log    []string
-	failed bool
-}
-
-func (t *testT) FailNow() {
-	t.failed = true
-}
-
-func (t *testT) Failed() bool {
-	return t.failed
-}
-
-func (t *testT) LogString() string {
-	return strings.Join(t.log, "\n")
-}
-
-func (t *testT) Log(args ...interface{}) {
-	t.log = append(t.log, fmt.Sprint(args...))
 }
